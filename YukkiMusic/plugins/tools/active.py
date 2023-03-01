2db942d8322632d0b7749f1bd45fd06ada6cd651
Hamam22 <117502176+Hamam22@users.noreply.github.com>
2023-03-01 13:54:24 +0700

    Update active.py

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -75,3 +75,10 @@ async def activevi_(_, message: Message):
             f"**Active Video Calls:-**\n\n{text}",
             disable_web_page_preview=True,
         )
+
+@app.on_message(filters.command("active", [".", "!", "/"]) & SUDOERS)
+async def active(_, message: Message):
+    ms = len(await get_active_chats())
+    vd = len(await get_active_video_chats())
+    await app.send_message(message.chat.id, 
+        f"🎧 Active Chat:\n\n• Music: {ms}\n• Video: {vd}")
